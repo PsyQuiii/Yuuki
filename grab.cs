42160960af3bff297d8d6c4ade54aad61cf5aedc
PsyQuiii <122717510+PsyQuiii@users.noreply.github.com>
2023-03-13 16:43:25 +0700

    Add files via upload

---
+++ grab.cs
@@ -0,0 +1,16 @@
+﻿class program{
+static void Main(string[] args){
+    Console.WriteLine("input Day of Week :");
+    int Day = int.Parse(Console.ReadLine());
+    if( Day == 8 ||Day == 9 ||Day == 10 ||Day == 11 ||Day == 12 ||Day == 13);{
+        Console.Write("pls put 1-7");
+       }
+     Console.WriteLine("input time :");
+    string time = Console.ReadLine();
+    Console.WriteLine("select menu :");
+    bool check = false;
+    int menu = int.Parse(Console.ReadLine());
+    if( breakfastset == 0 ||weekend == 0 ||coffee == 0 );
+       Console.WriteLine("end");
+}
+}
---
+++ nayook.cs
@@ -0,0 +1,7 @@
+class program2{
+    static void Mainn(string[] args){
+    Console.WriteLine("input number :");
+    int password = int.Parse(Console.ReadLine()); 
+   Console.WriteLine("Not Much Overlapping");  
+    }  
+    }
